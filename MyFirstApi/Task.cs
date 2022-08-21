d0f0dc990af985a652373126481a818e5cb96a9a
Orazali Adambayev <28313@iitu.edu.kz>
2022-08-21 18:31:03 +0600

    Добавьте файлы проекта.

---
+++ MyFirstApi/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstApi
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
