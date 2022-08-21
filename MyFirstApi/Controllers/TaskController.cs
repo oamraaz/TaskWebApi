fefc4c9d930ab51d76c95a195e4a1faa3f7b22ec
Orazali Adambayev <28313@iitu.edu.kz>
2022-08-21 21:07:26 +0600

    Added CRUP Operations

--- MyFirstApi/Controllers/TaskController.cs
+++ MyFirstApi/Controllers/TaskController.cs
@@ -6,6 +6,72 @@ namespace MyFirstApi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class TaskController : ControllerBase
-    {
+    { 
+        private static List<Task> tasks = new List<Task>
+            {
+                new Task {
+                    Id = 1,
+                    Name = "Add CRUD",
+                    Description = "Create, Read, Update, Delete",
+                }
+            };
+
+        private readonly DataContext _context;
+
+        public TaskController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Task>>> Get()
+        {
+            return Ok(await _context.tasks.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Task>> GetById(int id)
+        {
+            var task = await _context.tasks.FindAsync(id);
+            if (task == null)
+                return BadRequest("Task is not found");
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Task>>> AddTask(Task task)
+        {
+            _context.tasks.Add(task);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.tasks.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Task>>> UpdateTask(Task request)
+        {
+            var task = await _context.tasks.FindAsync(request.Id);
+            if (task == null)
+                return BadRequest("Task is not found");
+
+            task.Name = request.Name;
+            task.Description = request.Description;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.tasks.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Task>> DeleteById(int id)
+        {
+            var task = await _context.tasks.FindAsync(id);
+            if (task == null)
+                return BadRequest("Task is not found");
+
+            _context.tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.tasks.ToListAsync());
+        }
     }
 }
---
+++ MyFirstApi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Task> tasks { get; set; }
+    }
+}
